bc0f71fc6b59f0bbee374bd1354d54512bfe3633
Jayson San Agustin <Jayson San Agustin>
2017-05-25 02:01:08 +0800

    Initial Commit

---
+++ src/com/coreservelets/CoreServelets.java
@@ -0,0 +1,22 @@
+package com.coreservelets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Jayson San Agustin on 25/05/2017.
+ */
+@WebServlet(name = "CoreServelets")
+public class CoreServelets extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Jayson San Agustin
+  Date: 25/05/2017
+  Time: 1:15 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  This is my Home Page!
+  </body>
+</html>
